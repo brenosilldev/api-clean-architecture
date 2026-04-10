ed53b5223aaf0456dc151b1552122e9b5f9feed0
Breno Silva <brenosill@hotmail.com>
2026-04-10 16:14:49 -0300

    refactor: Refatorado para pegar o help de user-data

--- src/users/domain/entities/__tests__/unit/user.entity.spec.ts
+++ src/users/domain/entities/__tests__/unit/user.entity.spec.ts
@@ -1,16 +1,12 @@
 import { UserEntity, UserProps } from '../../user.entity';
-import { faker } from '@faker-js/faker';
+import { UserDataBuilder } from '../../../testing/helpers/user-data-builders';
 
 describe('UserEntity', () => {
     let user: UserEntity;
     let props: UserProps;
 
     beforeEach(() => {
-        props = {
-            name: faker.person.firstName(),
-            email: faker.internet.email(),
-            password: faker.internet.password(),
-        };
+        props = UserDataBuilder({});
 
         user = new UserEntity(props);
     });
@@ -20,5 +16,29 @@ describe('UserEntity', () => {
         expect(user.props.email).toBe(props.email);
         expect(user.props.password).toBe(props.password);
         expect(user.props.createdAt).toBe(props.createdAt);
+    });
+
+    it('should be able to get createdAt', () => {
+        expect(user.createdAt).toBeDefined();
+        expect(user.createdAt).toBe(props.createdAt);
+        expect(user.createdAt).toBeInstanceOf(Date);
+    });
+
+    it('should be able to get name', () => {
+        expect(user.name).toBeDefined();
+        expect(user.name).toEqual(props.name);
+        expect(user.name).toEqual(expect.any(String)); // Verifica se o nome é uma string
+    });
+
+    it('should be able to get email', () => {
+        expect(user.email).toBeDefined();
+        expect(user.email).toEqual(props.email);
+        expect(user.email).toEqual(expect.any(String));
+    });
+
+    it('should be able to get password', () => {
+        expect(user.password).toBeDefined();
+        expect(user.password).toEqual(props.password);
+        expect(user.password).toEqual(expect.any(String));
     });
 });

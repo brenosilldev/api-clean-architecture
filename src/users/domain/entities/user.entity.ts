20c926b2552143efefb962ab4113e87b81e93590
Breno Silva <brenosill@hotmail.com>
2026-04-13 20:23:26 -0300

    feat: Criado sets e updates

--- src/users/domain/entities/user.entity.ts
+++ src/users/domain/entities/user.entity.ts
@@ -6,7 +6,7 @@ import { Entity } from '../../../shared/domain/entities/entity';
 // Esse tipo é passado como genérico para Entity<UserProps>,
 // informando quais campos essa entidade possui.
 export type UserProps = {
-    id?: string;       // Opcional — usado apenas ao reconstruir um usuário do banco
+    // id?: string;       // Opcional — usado apenas ao reconstruir um usuário do banco
     name: string;      // Nome do usuário (obrigatório)
     email: string;     // E-mail do usuário (obrigatório)
     password: string;  // Senha do usuário (obrigatório)
@@ -19,7 +19,7 @@ export type UserProps = {
 // de forma controlada (via getters), sem expor a estrutura interna diretamente.
 export class UserEntity extends Entity<UserProps> {
     constructor(
-        public props: UserProps,
+        public readonly props: UserProps,
         id?: string, // ID opcional — se não passar, a classe base gera um UUID novo
     ) {
         // Chama o construtor da classe pai (Entity), passando as props e o id.
@@ -37,9 +37,17 @@ export class UserEntity extends Entity<UserProps> {
         return this.props.createdAt;
     }
 
+    updateName(value: string): void {
+        this.props.name = value;
+    }
+
     // Getter para acessar o nome do usuário.
     get name() {
         return this.props.name;
+    }
+
+   private set name(value: string) {
+        this.props.name = value;
     }
 
     // Getter para acessar o e-mail do usuário.
@@ -50,5 +58,9 @@ export class UserEntity extends Entity<UserProps> {
     // Getter para acessar a senha do usuário.
     get password() {
         return this.props.password;
+    }
+
+    private set password(value: string) {
+        this.props.password = value;
     }
 }

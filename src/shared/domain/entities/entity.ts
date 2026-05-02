0050292231738b3eeddb876e81d5e66fb9ec719c
Breno Silva <brenosill@hotmail.com>
2026-05-02 01:32:25 -0300

    chore: Alterado linhas

--- src/shared/domain/entities/entity.ts
+++ src/shared/domain/entities/entity.ts
@@ -1,20 +1,20 @@
 import { randomUUID as generateId } from 'node:crypto';
 
-export abstract class Entity<Props = any> {
-    public readonly _id: string;
+export abstract class Entity<Props = any> {// Classe abstrata para entidades
+    public readonly _id: string; // ID único da entidade
 
-    public readonly props: Props;
+    public readonly props: Props; // Propriedades da entidade
 
-    constructor(props: Props, id?: string) {
+    constructor(props: Props, id?: string) { // Construtor da entidade
         this.props = props;
         this._id = id ?? generateId();
     }
 
-    get id() {
-        return this._id;
+    get id() { // Getter para o ID da entidade
+        return this._id; // Getter para o ID da entidade
     }
 
     toJSON(): Required<Props & { id: string }> {
-        return this.props as Required<Props & { id: string }>;
+        return this.props as Required<Props & { id: string }>; // Método para converter a entidade para JSON
     }
 }

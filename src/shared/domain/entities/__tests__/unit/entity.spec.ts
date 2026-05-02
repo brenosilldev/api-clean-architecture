59460c3a44719638f86fae7992380a72ea5b4d14
Breno Silva <brenosill@hotmail.com>
2026-05-02 01:31:54 -0300

    raw: Removido linhas

--- src/shared/domain/entities/__tests__/unit/entity.spec.ts
+++ src/shared/domain/entities/__tests__/unit/entity.spec.ts
@@ -1,9 +1,6 @@
 // Importa a classe abstrata Entity para testar seus comportamentos base.
 import { UserDataBuilder } from '@/users/domain/testing/helpers/user-data-builders';
 import { Entity } from '../../entity';
-import  { UserEntity, UserProps } from '@/users/domain/entities/user.entity';
-import { en } from '@faker-js/faker';
-
 
 type StubProps = {
     props1: string;
@@ -13,18 +10,12 @@ type StubProps = {
 // Classe stub para testar a classe Entity.
 class StubEntity extends Entity<StubProps> {}
 
-
-
-
-
 describe('Entity', () => {
     it('should be able to create an entity', () => {
-      const props = { props1: 'props1 value', props2: 10 };
-      const entity = new StubEntity(props);
+        const props = { props1: 'props1 value', props2: 10 };
+        const entity = new StubEntity(props); // Cria uma nova instância da entidade stub
 
-      expect(entity.props.props1).toStrictEqual(props.props1);
-      expect(entity.id).not.toBeNull();
+        expect(entity.props).toStrictEqual(props); // Verifica se as propriedades da entidade são iguais aos valores passados
+        expect(entity.id).not.toBeNull(); // Verifica se o ID da entidade não é nulo
     });
-
-  
 });
